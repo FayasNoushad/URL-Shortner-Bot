65470c5c50fe63b585e469387778282249af4aa4
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-07 11:21:13 +0530

    Added bit.ly shorten feature

--- plugins/shortner.py
+++ plugins/shortner.py
@@ -2,7 +2,17 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyshorteners import Shortener
 
+BITLY_API = os.environ["BITLY_API"]
+
 @FayasNoushad.on_message(filters.private & filters.regex(r'https?://[^\s]+'))
 async def short(bot, update):
     link = update.matches[0].group(0)
     shorten_urls = "**--Shorted URLs--**\n"
+    
+    if BITLY_API:
+        try:
+            s = Shortener(api_key=BITLY_API)
+            url = s.bitly.short(link)
+            shorten_urls += f"\n**Bit.ly :-** {url}"
+        except Exception as error:
+            print(error)
