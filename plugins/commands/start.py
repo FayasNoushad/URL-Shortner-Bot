cb0676fc37d68144c6a5a77297c1bd6dc9f7c843
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-07 10:30:57 +0530

    Create start.py

---
+++ plugins/commands/start.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+START_TEXT = """
+Hello {}, I am a link shortner telegram bot.
+
+Made by @FayasNoushad
+"""
+
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+
+@FayasNoushad.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True
+    )
