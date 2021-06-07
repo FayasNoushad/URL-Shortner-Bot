e4430973ac33c235498749c6eb2e3650eaeeab2d
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-07 20:42:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ FayasNoushad = Client(
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
-    plugins = dict(root="plugins")
+    plugins = dict(
+        root="plugins"
+    )
 )
 
 FayasNoushad.run()
