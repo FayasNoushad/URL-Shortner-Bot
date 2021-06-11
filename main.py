a33fd63fcf879ff79d617d9b71935855b5fb1c0e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-11 14:07:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from pyrogram import Client
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
+
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-FayasNoushad = Client(
-    "URL-Shortner-Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
+if __name__ == "__main__":
+    bot_token = os.environ["BOT_TOKEN"]
+    api_id = int(os.environ["API_ID"])
+    api_hash = os.environ["API_HASH"]
     plugins = dict(
         root="plugins"
     )
-)
-
-if __name__ == "__main__":
+    FayasNoushad = Client(
+        "URL-Shortner-Bot",
+        bot_token=bot_token,
+        api_id=api_id,
+        api_hash=api_hash,
+        plugins = plugins
+    )
     FayasNoushad.run()
