a0a103e9a2e927e4a444766e66a16302a3f93282
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-11 16:06:06 +0530

    Update shortner.py

--- plugins/shortner.py
+++ plugins/shortner.py
@@ -131,9 +131,9 @@ async def short(bot, update):
     
     # NullPointer shorten
     try:
-        s = Shortener(domain='https://ttm.sh')
+        s = Shortener(domain='https://0x0.st')
         url = s.nullpointer.short(link)
-        shorten_urls += f"\n**ttm.sh :-** {url}"
+        shorten_urls += f"\n**0x0.st :-** {url}"
     except Exception as error:
         print(f"NullPointer error :- {error}")
     
